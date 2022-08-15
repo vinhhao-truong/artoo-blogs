e1dfc93f1c2d9308ed864806c794cf225a222929
vinhhao-truong <vinhhao.truong.52@gmail.com>
2022-08-15 20:50:40 +1000

    Release v1.0.5

--- server/index.js
+++ server/index.js
@@ -31,7 +31,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(3001, () => {
-  console.log("Server on 3001");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server on " + PORT);
   console.log();
 });
