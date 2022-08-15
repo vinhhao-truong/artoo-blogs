3abd602d1d8a1b2844b069f7828a15439a3098a2
vinhhao-truong <vinhhao.truong.52@gmail.com>
2022-08-16 01:14:38 +1000

    Release v1.0.6

--- client/src/components/fragments/Helmet.js
+++ client/src/components/fragments/Helmet.js
@@ -3,14 +3,50 @@ import { Helmet } from "react-helmet";
 const MainHelmet = () => {
   return (
     <Helmet>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="../../../public/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="../../../public/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="../../../public/favicon-16x16.png"
+      />
+      <link rel="manifest" href="../../../public/site.webmanifest" />
       <title>Artoo Blogs</title>
     </Helmet>
-  )
-}
+  );
+};
 
 const ChildHelmet = ({ title }) => {
   return (
     <Helmet>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="../../../public/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="../../../public/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="../../../public/favicon-16x16.png"
+      />
+      <link rel="manifest" href="../../../public/site.webmanifest" />
       <title>{title} | Artoo Blogs</title>
     </Helmet>
   );
